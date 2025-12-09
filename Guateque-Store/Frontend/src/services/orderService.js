202a77d851be33775db853695df23ccc0d6686df
Adrianbien43 <adribienmp43@gmail.com>
2025-12-09 23:31:58 +0100

    Fuera

--- Guateque-Store/Frontend/src/services/orderService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8080/api/pedidos';
-
-export const getPedidos = () => axios.get(API_URL);
-export const createPedido = (pedido) => axios.post(API_URL, pedido);
-export const updatePedido = (id, pedido) => axios.put(`${API_URL}/${id}`, pedido);
-export const deletePedido = (id) => axios.delete(`${API_URL}/${id}`);
--- Guateque-Store/Frontend/src/services/warehouseService.js
+++
@@ -1,9 +0,0 @@
-// src/services/warehouseService.js
-import axios from 'axios';
-
-const API_URL = 'http://localhost:8080/api';
-
-export const getAlmacenes = () => axios.get(`${API_URL}/almacenes`);
-export const createAlmacen = (datos) => axios.post(`${API_URL}/almacenes`, datos);
-export const updateAlmacen = (id, datos) => axios.put(`${API_URL}/almacenes/${id}`, datos);
-export const deleteAlmacen = (id) => axios.delete(`${API_URL}/almacenes/${id}`);
