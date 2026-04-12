56eb80c8072f6f6ca30116da3d8fc3101f7ad55e
Adrianbien43 <adribienmp43@gmail.com>
2026-04-12 15:05:19 +0200

    Creacion de un modelo compartido con TipoTelefono
    feat: Cree un enum para centralizar el tipo de telefono, ya que considere que existia duplicación entre los Usuarios y Proveedores. Lo que establecemos son los tipos de teléfonos (Movil, Fijo).

---
+++ Backend/src/main/java/com/guatequestore/backend/shared/model/TipoTelefono.java
@@ -0,0 +1,18 @@
+package com.guatequestore.backend.shared.model;
+
+/**
+ * Enumeración que define los tipos de teléfono disponibles en el sistema.
+ * 
+ * Valores:
+ * - MOVIL: Teléfono móvil
+ * - FIJO: Teléfono fijo
+ * 
+ * Se utiliza tanto en TelefonoUsuario como en TelefonoProveedor.
+ * 
+ * @author Adrian Bienvenido
+ * @version 1.0.0
+ */
+public enum TipoTelefono {
+    MOVIL,
+    FIJO
+}
